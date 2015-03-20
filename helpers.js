a8c70b65eae4244c2183c6f004abf99c129ccf53
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-03-20 02:55:30 -0400

    Better function names

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
-function myFunction(){
+document.getElementById('myFile').addEventListener('change', getFileInfo, false);
+function getFileInfo(){
     var x = document.getElementById("myFile");
     var txt = "";
     if ('files' in x) {
@@ -25,5 +26,5 @@ function myFunction(){
             txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead.
         }
     }
-    document.getElementById("demo").innerHTML = txt;
+    document.getElementById("data").innerHTML = txt;
 }
