a1f9be66e18d4ae80e55fa765f2f96204606f01d
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-03-20 01:46:50 -0400

    Able to send and receive data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//globally defined variables
+var stringReceived = '';
+var connectionId = null;
+
 var onGetDevices = function(ports) {
-  console.log("hehe");
   for (var i=0; i<ports.length; i++) {
     console.log(ports[i].path);
-    //connect to serial port
   }
   //cant connect to when other services are polling it (i.e. coolterm) :(
   chrome.serial.connect("/dev/tty.usbmodem1411", {bitrate: 9600}, onConnect);
@@ -11,12 +13,43 @@ var onGetDevices = function(ports) {
 var onConnect = function(connectionInfo) {
    // The serial port has been opened. Save its id to use later.
    console.log(connectionInfo);
-  //_this.connectionId = connectionInfo.connectionId;
-  // Do whatever you need to do with the opened port.
+   connectionId = connectionInfo.connectionId;
+   console.log(connectionId);
+   //try to conenct to the port
+   writeSerial("M");
+   writeSerial("E");
+   writeSerial("F");
+  chrome.serial.onReceive.addListener(onReceiveCallback);
 };
-// Connect to the serial port /dev/ttyS01
+
+var onReceiveCallback = function(info) {
+    if (info.connectionId == connectionId && info.data) {
+      console.log(info.data);
+      var str = String.fromCharCode.apply(null, new Uint16Array(info.data));
+      console.log(str);
+      if (str.charAt(str.length-1) === '\n') {
+        stringReceived += str.substring(0, str.length-1);
+        onLineReceived(stringReceived);
+        stringReceived = '';
+      } else {
+        stringReceived += str;
+      }
+    }
+  };
 
 
+var writeSerial=function(str) {
+  chrome.serial.send(connectionId, convertStringToArrayBuffer(str), onReceiveCallback);
+};
+// Convert string to ArrayBuffer
+var convertStringToArrayBuffer=function(str) {
+  var buf=new ArrayBuffer(str.length);
+  var bufView=new Uint8Array(buf);
+  for (var i=0; i<str.length; i++) {
+    bufView[i]=str.charCodeAt(i);
+  }
+  return buf;
+};
 
 $( document ).ready(function() {
    chrome.serial.getDevices(onGetDevices);
