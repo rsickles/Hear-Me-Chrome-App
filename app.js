1e28e4a252b914c2f1ebeeb37ba943b462a818ee
Ryan Sickles <rsickles@andrew.cmu.edu>
2015-04-10 00:11:06 -0400

    WIP with sending .wav file as binary string to chrome Serial

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var onGetDevices = function(ports) {
     console.log(ports[i].path);
   }
   //cant connect to when other services are polling it (i.e. coolterm) :(
-  chrome.serial.connect("/dev/tty.usbmodem1411", {bitrate: 9600}, onConnect);
+  chrome.serial.connect("/dev/tty.usbmodem1421", {bitrate: 9600}, onConnect);
 };
 
 var onConnect = function(connectionInfo) {
@@ -69,3 +69,62 @@ function bufferToString( buf ) {
 $( document ).ready(function() {
    chrome.serial.getDevices(onGetDevices);
 });
+
+//dealing with the file below
+document.getElementById('myFile').addEventListener('change', getFileInfo, false);
+function getFileInfo(){
+    var x = document.getElementById("myFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length === 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                //do something with the file
+                console.log(file);
+                var reader = new FileReader();
+                reader.readAsBinaryString(file);
+                reader.onload = function(event) {
+                // file is loaded, contents are in event.target.result
+                // do something with it!
+                  var wav_string = "0" + event.currentTarget.result;
+                  var buf = new ArrayBuffer(wav_string.length*4); // 4 bytes for each char
+                  var bufView = new Uint8Array(buf);
+                  //intiaties transfer
+                  writeSerial("I");
+                  //holds number of stories
+                  writeSerial(String.fromCharCode(1));
+                  //holds starting location
+                  writeSerial(String.fromCharCode((0 >> 24) & 0xFF));
+                  writeSerial("D");
+                  writeSerial(wav_string);
+                  bufView[0] = String.fromCharCode((0 >> 24) & 0xFF);
+                  for (var i=1, strLen=wav_string.length; i<strLen; i++) {
+                    bufView[i] = wav_string.charCodeAt(i);
+                  }
+                  console.log(bufView);
+                  //chrome.serial.send(connectionId, bufView, onReceiveCallback);
+                }
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    }
+    else {
+        if (x.value === "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead.
+        }
+    }
+    document.getElementById("data").innerHTML = txt;
+}
+
+
